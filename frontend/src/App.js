e1cb612fb364d1b251cb68373002aefb6488d56a
Francisco Delzon <telasperalta@gmail.com>
2022-02-23 01:06:34 -0300

    Partiendo con el proyecto

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Componentes/Header';
+import Footer from './Componentes/Footer';
+
+function App() {
+  return (
+    <>
+        <Header/>
+        <main>
+
+        </main>
+        <Footer/>
+    </>
+  );
+}
+
+export default App;
